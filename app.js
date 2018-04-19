cb51d436b510715e7807416297ec7e68576c8719
Local Admin <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-04-19 14:20:21 +0100

    changed app port

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 8081);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
