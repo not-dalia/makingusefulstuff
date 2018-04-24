72f0457ce6465d2bddabb79c2dac68d8530e49ae
Dalia <daliah1991@gmail.com>
2018-04-24 08:11:32 +0100

    added image upload to s3

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var aws = require('aws-sdk')
+var multer = require('multer')
+var multerS3 = require('multer-s3')
+
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usernameRouter = require('./routes/username');
 
 var app = express();
+var s3 = new aws.S3({ params: { Bucket: process.env.S3_BUCKET_NAME } })
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,20 +32,41 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/username', usernameRouter);
 
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        metadata: function(req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function(req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+})
+
+app.post('/upload', upload.single('photo'), function(req, res, next) {
+    res.send('Successfully uploaded ' + req.files.length + ' files!')
+})
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
