5b0123e6274a90feeb8f34342a883d0bd6a1665c
Local Admin <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-04-24 11:59:55 +0100

    connected to AWS RDS

--- main.js
+++ main.js
@@ -6,15 +6,29 @@ var logger = require('morgan');
 var aws = require('aws-sdk')
 var multer = require('multer')
 var multerS3 = require('multer-s3')
+var mysql = require('mysql');
 
-require('dotenv').config()
+
+const NOUNS = ["3DPrinter", "Maker", "Inventor", "Creator", "Scientist", "Engineer", "Designer", "Programmer", "Robot", "LaserCutter", "Knitter", "Chip", "Ink", "Electron", "Proton", "Artist", "Arduino", "Hacker", "Button", "Sensor", "PowerSupply", "Transistor", "Resistor", "Capacitor", "LED", "Coil", "Motor", "Actuator", "Ribbon", "Pin", "Scissors", "Filament", "Thimble", "Needle", "Hammer"];
+const ADJECTIVES = ["Antimatter", "Crafty", "Terrific", "Ubiquitous", "Rebellious", "Efficacious", "Fastidious", "Jocular", "Playful", "Nefarious", "Zealous", "Ambiguous", "Auspicious", "Berserk", "Bustling", "Calculating", "Colossal", "Decisive", "Dynamic", "Elastic", "Ethereal", "Exuberant", "Fabulous", "Fearless", "Grandiose", "Harmonious", "Hypnotic", "Incandescent", "Invincible", "Nebulous", "Nimble", "Omniscient", "Quirky", "Stupendous", "Thundering", "Whimsical", "Malevolent", "Spooky", "Majestic", "Epic", "Humble"];
+
+
+//aws.config.loadFromPath('./config.json');
+var connection = mysql.createConnection({
+    host: process.env.AWS_DB_HOST,
+    user: process.env.AWS_DB_USER,
+    password: process.env.AWS_DB_PASS,
+    database: process.env.AWS_DB_NAME
+});
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var usernameRouter = require('./routes/username');
+var postsRouter = require('./routes/posts');
+var uploadsRouter = require('./routes/uploads');
 
 var app = express();
-var s3 = new aws.S3({ params: { Bucket: process.env.S3_BUCKET_NAME } })
+var s3 = new aws.S3({ params: { Bucket: process.env.S3_BUCKET_NAME } });
+
 
 
 // view engine setup
@@ -29,8 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/username', usernameRouter);
+app.use('/uploads', uploadsRouter);
 
 
 var upload = multer({
@@ -39,27 +52,86 @@ var upload = multer({
         bucket: process.env.S3_BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         acl: 'public-read',
-        metadata: function(req, file, cb) {
+        metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
-        key: function(req, file, cb) {
+        key: function (req, file, cb) {
             cb(null, Date.now().toString())
         }
     })
 })
 
-app.post('/upload', upload.single('photo'), function(req, res, next) {
-    res.send('Successfully uploaded ' + req.files.length + ' files!')
+let generateNames = function (count) {
+    let names = [];
+    while (names.length < count) {
+        let name = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)] + NOUNS[Math.floor(Math.random() * NOUNS.length)];
+        if (names.indexOf(name) < 0)
+            names.push(name);
+    }
+    return names;
+}
+
+app.post('/upload', upload.single('photo'), function (req, res, next) {
+    try {
+        if (!req.file || !req.file.location) throw new Error('There was a problem with the file upload')
+        let names = []
+
+        names = generateNames(100);
+        connection.query('SELECT name FROM posts WHERE name = ? GROUP BY name', names, function (error, results, fields) {
+            if (error) {
+                res.json({ success: false, error: error });
+                return;
+            }
+            console.log(results);
+            results.forEach(el => {
+                if (names.indexOf(el.name) >= 0) names.splice(names.indexOf(el.name), 0)
+            });
+
+            if (names.length == 0) {
+                res.json({ success: false, error: 'Could not generate a name. Please try again.' });
+                return;
+            }
+
+            connection.query('INSERT INTO posts SET ?', { name: names[0], imageLocation: req.file.location }, function (error, results, fields) {
+                if (error) res.json({ success: false, error: error });
+                else res.json({ success: true, path: req.file.location, name: names[0] });
+            });
+        })
+    } catch (err) {
+        res.json({ success: false, error: err });
+    }
 })
+
+app.get('/generateName/', function (req, res, next) {
+    let name = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)] + NOUNS[Math.floor(Math.random() * NOUNS.length)];
+    res.json({ name });
+});
+
+app.get('/posts/:postId', function (req, res, next) {
+    try {
+        connection.query('SELECT * FROM posts WHERE name = ?', req.params.postId, function (error, results, fields) {
+            try {
+                if (error) throw error;
+                if (results.length == 0) throw new Error();
+                res.render('post', { name: results[0].name, image: results[0].imageLocation });
+            } catch (err) {
+                next(createError(404));
+            }
+        })
+    } catch (err) {
+        next(createError(404));
+    }
+
+});
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
